c92881ac1bfe8268636d3dabde77906a45dba5a0
jpovvacor96 <jpovvacor96@gmail.com>
2023-12-13 16:39:49 +0100

    angular-firebase-1: Iniciado el primer proyecto de Angular sincronizado con Google Firebase

---
+++ angular-firebase-1/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    apiKey: "AIzaSyC3R5cc4nMDN6IRxvk45Cqj7UX6yMZwkSk",
+    authDomain: "angular-firebase-1-954e2.firebaseapp.com",
+    databaseURL: "https://angular-firebase-1-954e2-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "angular-firebase-1-954e2",
+    storageBucket: "angular-firebase-1-954e2.appspot.com",
+    messagingSenderId: "707221706019",
+    appId: "1:707221706019:web:393b227d4f3e8ba99da896"
+};
